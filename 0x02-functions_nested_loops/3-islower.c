3219782143ed0646da12467e6c85625127369adf
dmowino1 <dmowino@hotmail.com>
2022-10-19 15:13:45 -0700

    3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - Controls if a character is lower case
+ * @c: character to be verified
+ * Return: 1 if true, 0 if false
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+
+
+	return (0);
+}
